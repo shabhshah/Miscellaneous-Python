4d900a3fbf7784add6714916bc6776429633e8d7
Rishabh <rishabhshah28@gmail.com>
2016-04-04 17:59:52 -0400

    Initial commit. Program works. Did what it needed to do.

---
+++ packagereader.py
@@ -0,0 +1,17 @@
+#Rishabh Shah
+#2016
+#Creating a file to batch install packages on Ubuntu
+
+fh = open("package.txt","a")
+fh.write("sudo apt-get install ")
+
+for line in open("packages.txt"):
+    line = line.strip()
+
+    parts = line.split()
+
+    importantPart = parts[0]
+
+    fh.write(importantPart + " ")
+
+fh.close()
